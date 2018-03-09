7c3f701603584911b992ce0445b00b1a462ea060
Anna Becker <anna.r.becker@gmail.com>
2018-03-08 20:16:35 -0800

    Add reducer for task: find the total sales value across all the stores, and the total number of sales. Assume there is only one reducer

---
+++ requcer_q3.py
@@ -0,0 +1,19 @@
+import sys
+# Data from mapper
+# Store     Sale
+# San Jose  12.99
+
+salesTotal = 0
+totalNumberOfSales = 0
+
+# Find the total sales value across all the stores, and the total number of sales. Assume there is only one reducer
+for line in sys.stdin:
+    data = line.strip().split("\t")
+    if len(data) != 2:
+        continue
+
+    store, sale = data
+    salesTotal =+ float(sale)
+    totalNumberOfSales += 1
+
+    print "{0}\t{1}".format(salesTotal, totalNumberOfSales)
